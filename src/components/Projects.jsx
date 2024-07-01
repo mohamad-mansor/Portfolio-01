46c55f670405f81198b0d41a9c8feaf76c4e8815
Mohamad <mohamad.alhajr@dci-student.org>
2024-07-01 10:28:23 +0200

    p

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
   return (
     <section id="projects" className="py-20">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-10">Projects</h2>
+        <h2 className="text-3xl font-bold mb-10">Selected Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {/* Add your project cards here */}
           <ProjectCard
@@ -29,7 +29,7 @@ const Projects = () => {
           <ProjectCard
             image="https://raw.githubusercontent.com/mohamad-mansor/Portfolio-00/main/Screenshot%20from%202024-02-07%2012-42-29.png"
             github="https://github.com/mohamad-mansor/Portfolio-00"
-            live="https://mohamad-mansor.github.io/PORTFOLIO/"
+            live="https://mohamad-mansor.github.io/Portfolio-00/"
           />
           <ProjectCard
             image="https://raw.githubusercontent.com/mohamad-mansor/TO-DO-List/main/images/Screenshot%20from%202024-06-27%2012-33-49.png"
